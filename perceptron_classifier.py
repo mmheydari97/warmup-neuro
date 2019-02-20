d75d593c1f76275c7230f5596ee00af54422acc2
mmheydari97 <mmheydari97@pop-os.localdomain>
2019-02-20 09:27:56 +0330

    lonely perceptron

---
+++ perceptron_classifier.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import neurolab as nl
+
+
+# Load input data
+text = np.loadtxt('data_perceptron.txt')
+
+# Separate data points and Labels
+data = text[:, :2]
+labels = text[:, 2].reshape((text.shape[0], 1))
+
+# Plot input data
+plt.figure()
+plt.scatter(data[:, 0], data[:, 1])
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.title('Input data')
+
+# Define minimum and maximum values for each dimension
+dim1_min, dim1_max, dim2_min, dim2_max = 0, 1, 0, 1
+
+# Bumber of neurons in the output layer
+num_output = labels.shape[1]
+
+# Define a perceptron with 2 input neurons
+dim1 = [dim1_min, dim1_max]
+dim2 = [dim2_min, dim2_max]
+perceptron = nl.net.newp([dim1, dim2], num_output)
+
+# Train the perceptron using the data
+err = perceptron.train(data, labels, epochs=100, show=20, lr=0.03)
+
+# Plot the training progress
+plt.figure()
+plt.plot(err)
+plt.xlabel('Number of epochs')
+plt.ylabel('Training error')
+plt.title('Training error progress')
+plt.show()
